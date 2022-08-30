f2469de7b253d3a5d74d096c82e24fcc2cb56f9f
jerusalem <jerusalem@DESKTOP-DAIF0TH>
2022-08-30 09:27:01 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CascadingDropDownList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadingDropDownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        ListEntities List = new ListEntities();
+
+        public ActionResult Index()
+        {
+            ViewBag.city=List.City_Table.ToList();
+            ViewBag.country = List.CountryTable.ToList();
+
+            // ViewBag.city = new SelectList(List.City_Table.ToList(), "City_id", "City_name");
+            //  ViewBag.country = new SelectList(List.CountryTable.ToList(), "Country_id", "Country_name");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult DisplayCountry(int cityid)
+        {
+
+
+            return Json(List.CountryTable.Where(c => c.Country_id == cityid).Select(c => new
+            {
+                Id = c.Country_id,
+                Name=c.Country_name,
+            }).ToList(),JsonRequestBehavior.AllowGet) ;
+        }
+
+    }
+}
---
+++ Models/CascadingClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CascadingDropDownList.Models
+{
+    public class CascadingClass
+    {
+        public int City_id { get; set; }
+        public string City_name { get; set; }
+
+        public int Country_id { get; set; }
+
+        public string Country_name { get; set; }
+
+        public int id  { get; set; }
+    } 
+}
